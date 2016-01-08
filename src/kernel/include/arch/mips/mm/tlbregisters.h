842c2171ecad76a5690d90b13425551c738e84f6
Davide Bettio <davide@uninstall.it>
2016-01-08 14:06:41 +0100

    mips/mm/tlbregisters.h: add probeTLB function.

--- src/kernel/include/arch/mips/mm/tlbregisters.h
+++ src/kernel/include/arch/mips/mm/tlbregisters.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
 
+#define TLB_MISSING_PAGE_MASK 0x80000000
+
 extern "C"
 {
     void updateTLB(unsigned long *pageTable, int pageDirectoryIndex, int pageTableIndex, uint8_t asid);
     uint8_t switchASID(uint8_t asid);
+    int probeTLB(const void *addr);
 }
